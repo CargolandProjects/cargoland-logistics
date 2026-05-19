ecd65a53c48ceca4aa0dccef3282d56f33337b4a
david <davidojo.dev@gmail.com>
2026-05-19 15:56:05 +0100

    feat(auth): implement signup page and shared UI components
    - Add auth layout and client-side signup page
    - Integrate react-hook-form with zod validation
    - Handle phone input parsing and country selection with custom components
    - Introduce reusable UI components: Command, CountryDropdown, Dialog, PhoneInput, Popover
    - Update global styles: add Montserrat for headings and .submit-button utility

---
+++ app/(auth)/signup/page.tsx
@@ -0,0 +1,501 @@
+"use client";
+
+import { EyeOff } from "@/components/icons";
+import { Button } from "@/components/ui/button";
+import { Country, CountryDropdown } from "@/components/ui/country-dropdown";
+import {
+  Field,
+  FieldDescription,
+  FieldError,
+  FieldGroup,
+  FieldLabel,
+  FieldLegend,
+  FieldSet,
+  FieldTitle,
+} from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import { PhoneInput, phoneSchema } from "@/components/ui/phone-input";
+import { useSignUp } from "@/lib/hooks/mutation/useAuth";
+import { zodResolver } from "@hookform/resolvers/zod";
+import IntlTelInput from "@intl-tel-input/react";
+import "intl-tel-input/styles";
+import { Check, Eye, EyeOffIcon } from "lucide-react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { Controller, useForm, useWatch } from "react-hook-form";
+import { toast } from "sonner";
+import z from "zod";
+
+const formSchema = z
+  .object({
+    firstName: z
+      .string()
+      .min(3, "First name must be at least 3 characters long")
+      .max(100, "First name must be less than 100 characters long")
+      .regex(
+        /^[a-zA-Z\s'-]+$/,
+        "First name can only contain letters, spaces, hyphens, and apostrophes"
+      ),
+    lastName: z
+      .string()
+      .min(3, "Last name must be at least 3 characters long")
+      .max(100, "Last name must be less than 100 characters long")
+      .regex(
+        /^[a-zA-Z\s'-]+$/,
+        "last name can only contain letters, spaces, hyphens, and apostrophes"
+      ),
+    email: z
+      .email("Enter a valid email address")
+      .max(100, "email must be less than 100 characters long"),
+
+    country: z.string("must provid a valid country"),
+    phoneNumber: phoneSchema,
+    // z.string()
+    //   .min(7, "Phone number is too short")
+    //   .max(15, "Phone number is too long")
+    //   .regex(/^\+?\d+$/, "Phone number must contain only digits")
+    password: z
+      .string()
+      .min(8, "Password must be at least 8 characters")
+      .max(20, "Password must not be more than 20 characters")
+      .regex(
+        /(?=.*[a-z])/,
+        "Password must contain at least one lowercase letter"
+      )
+      .regex(
+        /(?=.*[A-Z])/,
+        "Password must contain at least one uppercase letter"
+      )
+      .regex(/(?=.*\d)/, "Password must contain at least one number")
+      .regex(
+        /(?=.*[!@#$%^&*])/,
+        "Password must contain at least one special character"
+      ),
+    confirmPassword: z.string(),
+    termsAndCondition: z
+      .boolean()
+      .refine(
+        (val) => val === true,
+        "Please accept the terms and conditions to proceed"
+      ),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    error: "Passwords do not match",
+    path: ["confirmPassword"],
+  });
+
+type SignUp = z.infer<typeof formSchema>;
+
+export default function SignupPage() {
+  const router = useRouter();
+  const [selectedCountry, setSelectedCountry] = useState<Country | null>(null);
+  const [isVisible, setIsVisible] = useState({
+    createPassword: false,
+    confirmPassword: false,
+  });
+
+  //   console.log("Selected Country:", selectedCountry);
+
+  const { handleSubmit, control, setValue } = useForm<SignUp>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      firstName: "",
+      lastName: "",
+      email: "",
+      phoneNumber: "",
+      password: "",
+      confirmPassword: "",
+      termsAndCondition: false,
+    },
+  });
+
+  const password = useWatch({ control: control, name: "password" }) || "";
+  //   console.log("password: ", password);
+
+  const passwordReq = {
+    letterAndNumber: /(?=.*[a-z])/.test(password) && /(?=.*\d)/.test(password),
+    characterCount: password.length >= 8 && password.length <= 20,
+    specialCharacter: /(?=.*[!@#$%^&*])/.test(password),
+  };
+  //   console.log("passwordRequirements: ", passwordReq);
+
+  const onSubmit = (data: SignUp) => {
+    console.log("Signup Data: ", data);
+    const payload = { ...data, country: selectedCountry?.name };
+   
+  };
+
+  return (
+    <div className="mt-18.75 mb-40.75 ">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="p-6 bg-white rounded-lg max-w-[747px] mx-auto "
+      >
+        <FieldSet>
+          <div className="flex flex-col items-center">
+            <FieldTitle className="font-heading text-2xl font-bold leading-8">
+              Create Your CargoLand Account
+            </FieldTitle>
+            <FieldLegend className="mt-2 font-roboto text-brand-gray text-base font-normal leading-6">
+              Sign up to start shipping smarter and track every delivery with
+              ease.
+            </FieldLegend>
+          </div>
+
+          <FieldGroup className="mt-8 gap-6">
+            <div className="grid grid-cols-2 gap-4.5">
+              <Controller
+                name="firstName"
+                control={control}
+                render={({ field, fieldState }) => (
+                  <Field data-invalid={fieldState.invalid} className="gap-1">
+                    <FieldLabel htmlFor={field.name} className="form-label">
+                      First Name
+                    </FieldLabel>
+                    <Input
+                      {...field}
+                      id={field.name}
+                      aria-invalid={fieldState.invalid}
+                      placeholder="First Name"
+                      className="form-input"
+                    />
+                    {fieldState.invalid && (
+                      <FieldError
+                        errors={[fieldState.error]}
+                        className="form-error"
+                      />
+                    )}
+                  </Field>
+                )}
+              />
+
+              <Controller
+                name="lastName"
+                control={control}
+                render={({ field, fieldState }) => (
+                  <Field data-invalid={fieldState.invalid} className="gap-1">
+                    <FieldLabel htmlFor={field.name} className="form-label">
+                      Last Name
+                    </FieldLabel>
+                    <Input
+                      {...field}
+                      id={field.name}
+                      aria-invalid={fieldState.invalid}
+                      placeholder="Last Name"
+                      className="form-input"
+                    />
+                    {fieldState.invalid && (
+                      <FieldError
+                        errors={[fieldState.error]}
+                        className="form-error"
+                      />
+                    )}
+                  </Field>
+                )}
+              />
+            </div>
+
+            <Controller
+              name="email"
+              control={control}
+              render={({ field, fieldState }) => (
+                <Field data-invalid={fieldState.invalid}>
+                  <FieldLabel htmlFor={field.name} className="form-label">
+                    Email Address
+                  </FieldLabel>
+                  <Input
+                    {...field}
+                    id={field.name}
+                    type="email"
+                    aria-invalid={fieldState.invalid}
+                    placeholder="Email Address"
+                    className="form-input"
+                  />
+                </Field>
+              )}
+            />
+            <div className="flex gap-4.5">
+              <Controller
+                name="country"
+                control={control}
+                render={({ field, fieldState }) => (
+                  <Field
+                    data-invalid={fieldState.invalid}
+                    className="min-w-20 md:min-w-[108px] flex-1"
+                  >
+                    <FieldLabel className="form-label">Country</FieldLabel>
+                    <CountryDropdown
+                      defaultValue={field.value}
+                      onChange={(country) => {
+                        field.onChange(country.alpha2);
+                        setSelectedCountry(country);
+                        setValue("phoneNumber", country.countryCallingCodes[0]);
+                      }}
+                      className="form-input gap-3"
+                      slim
+                    />
+                    {fieldState.invalid && (
+                      <FieldError
+                        errors={[fieldState.error]}
+                        className="form-error"
+                      />
+                    )}
+                  </Field>
+                )}
+              />
+
+              <Controller
+                name="phoneNumber"
+                control={control}
+                render={({ field, fieldState }) => (
+                  <Field data-invalid={fieldState.invalid} className="gap-1">
+                    <FieldLabel htmlFor={field.name} className="form-label">
+                      Phone Number
+                    </FieldLabel>
+                    <PhoneInput
+                      {...field}
+                      value={field.value}
+                      onChange={field.onChange}
+                      defaultCountry={selectedCountry?.alpha2}
+                      onCountryChange={(country) => {
+                        setSelectedCountry(country as Country);
+                        if (!country) return;
+                        setValue("country", country?.alpha2);
+                      }}
+                      inline
+                      aria-invalid={fieldState.invalid}
+                      className="form-input border"
+                    />
+                    {fieldState.invalid && (
+                      <FieldError
+                        errors={[fieldState.error]}
+                        className="form-error"
+                      />
+                    )}
+                  </Field>
+                )}
+              />
+            </div>
+
+            <Controller
+              name="password"
+              control={control}
+              render={({ field, fieldState }) => (
+                <Field data-invalid={fieldState.invalid}>
+                  <FieldLabel htmlFor={field.name} className="form-label">
+                    Create Password
+                  </FieldLabel>
+                  <div className="relative">
+                    <Input
+                      {...field}
+                      id={field.name}
+                      type={isVisible.createPassword ? "text" : "password"}
+                      aria-invalid={fieldState.invalid}
+                      placeholder="Create Password"
+                      className="form-input"
+                    />
+                    <button
+                      onClick={() =>
+                        setIsVisible((prev) => ({
+                          ...prev,
+                          createPassword: !prev.createPassword,
+                        }))
+                      }
+                    >
+                      {isVisible.createPassword ? (
+                        <Eye className="size-6 text-slate-600/85 absolute right-4 top-1/2 -translate-y-1/2" />
+                      ) : (
+                        <EyeOff className="size-6 text-slate-600/85 absolute right-4 top-1/2 -translate-y-1/2" />
+                      )}
+                    </button>
+                  </div>
+                  {fieldState.invalid && (
+                    <FieldError
+                      errors={[fieldState.error]}
+                      className="form-error"
+                    />
+                  )}
+                </Field>
+              )}
+            />
+
+            <Controller
+              name="confirmPassword"
+              control={control}
+              render={({ field, fieldState }) => (
+                <Field data-invalid={fieldState.invalid}>
+                  <FieldLabel htmlFor={field.name} className="form-label">
+                    Confirm Password
+                  </FieldLabel>
+                  <div className="relative">
+                    <Input
+                      {...field}
+                      id={field.name}
+                      type={isVisible.confirmPassword ? "text" : "password"}
+                      aria-invalid={fieldState.invalid}
+                      placeholder="Confirm Password"
+                      className="form-input"
+                    />
+                    <button
+                      onClick={() =>
+                        setIsVisible((prev) => ({
+                          ...prev,
+                          confirmPassword: !prev.confirmPassword,
+                        }))
+                      }
+                    >
+                      {isVisible.confirmPassword ? (
+                        <Eye className="size-6 text-slate-600/85 absolute right-4 top-1/2 -translate-y-1/2" />
+                      ) : (
+                        <EyeOff className="size-6 text-slate-600/85 absolute right-4 top-1/2 -translate-y-1/2" />
+                      )}
+                    </button>
+                  </div>
+                  {fieldState.invalid && (
+                    <FieldError
+                      errors={[fieldState.error]}
+                      className="form-error"
+                    />
+                  )}
+                </Field>
+              )}
+            />
+          </FieldGroup>
+        </FieldSet>
+
+        <div className="mt-6">
+          <h3 className="font-roboto text-sm font-normal leading-5.25 uppercase text-slate-700 ">
+            Requirements:
+          </h3>
+
+          <div className="flex flex-wrap gap-y-3 gap-x-4 max-w-[348px]">
+            <div className="flex gap-2.5 items-center min-w-[148px]">
+              <Check
+                className={`${
+                  passwordReq.characterCount
+                    ? "text-cargo-success"
+                    : "text-brand-gray"
+                } size-4.5 `}
+              />
+              <p
+                className={`${
+                  passwordReq.characterCount
+                    ? "text-brand-black"
+                    : "text-brand-gray"
+                }`}
+              >
+                8 Characters (20 max)
+              </p>
+            </div>
+
+            <div className="flex gap-2.5 items-center min-w-[148px]">
+              <Check
+                className={`${
+                  passwordReq.letterAndNumber
+                    ? "text-cargo-success"
+                    : "text-brand-gray"
+                } size-4.5 `}
+              />
+              <p
+                className={`${
+                  passwordReq.letterAndNumber
+                    ? "text-brand-black"
+                    : "text-brand-gray"
+                }`}
+              >
+                1 letter and 1 number
+              </p>
+            </div>
+
+            <div className="flex gap-2.5 items-center min-w-[148px]">
+              <Check
+                className={`${
+                  passwordReq.specialCharacter
+                    ? "text-cargo-success"
+                    : "text-brand-gray"
+                } size-4.5 `}
+              />
+              <p
+                className={`${
+                  passwordReq.specialCharacter
+                    ? "text-brand-black"
+                    : "text-brand-gray"
+                }`}
+              >
+                1 special character (e.g #,*,%)
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <Controller
+          name="termsAndCondition"
+          control={control}
+          render={({ field, fieldState }) => (
+            <Field data-invalid={fieldState.invalid}>
+              <div className="flex items-center gap-4 mt-6">
+                <FieldLabel
+                  htmlFor={field.name}
+                  className="flex items-center gap-2 cursor-pointer"
+                >
+                  <Input
+                    id={field.name}
+                    type="checkbox"
+                    checked={field.value}
+                    onChange={(e) => field.onChange(e.target.checked)}
+                    aria-invalid={fieldState.invalid}
+                    className="peer hidden"
+                  />
+
+                  <div className="size-4.5 border-2 shrink-0 border-slate-600/90 rounded-[2px] peer-checked:bg-primary peer-checked:border-primary peer-checked:[&>svg]:block flex items-center justify-center">
+                    <svg
+                      className="hidden text-white"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeWidth="3"
+                    >
+                      <path d="M5 13l4 4L19 7" />
+                    </svg>
+                  </div>
+                </FieldLabel>
+                <FieldDescription className="text-slate-600/85 block">
+                  By signing up, you agree to our
+                  <Link
+                    href="/terms-conditions"
+                    className="text-primary underline-offset-[1.5px]! "
+                  >
+                    {" "}
+                    Terms of Service{" "}
+                  </Link>{" "}
+                  and
+                  <Link
+                    href="/privacy-policy"
+                    className="text-primary underline-offset-[1.5px]!"
+                  >
+                    {" "}
+                    Privacy Policy,{" "}
+                  </Link>
+                  and consent to receive important shipping updates from
+                  Cargoland Africa.
+                </FieldDescription>
+              </div>
+              {fieldState.invalid && (
+                <FieldError
+                  errors={[fieldState.error]}
+                  className="form-error"
+                />
+              )}
+            </Field>
+          )}
+        />
+
+        <Button type="submit" className="mt-6 submit-button">
+          Sign Up
+        </Button>
+      </form>
+    </div>
+  );
+}
