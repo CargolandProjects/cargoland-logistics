98e75242ee1058d7c97b5186dc8e651fdb2a1c72
david <davidojo.dev@gmail.com>
2026-06-22 20:30:44 +0100

    fix(legal pages): recreate privacy policy and  terms and conditions pages

--- app/(home)/privacy-policy/page.tsx
+++ app/(home)/privacy-policy/page.tsx
@@ -1,80 +1,121 @@
+export const metadata = {
+  title: "Privacy Policy",
+  description:
+    "Learn how Cargoland Logistics collects, uses, and protects your personal data in compliance with GDPR and Nigerian data laws.",
+};
+
+const policies = [
+  {
+    title: "Introduction",
+    paragraph: [
+      "Dolor fringilla nunc nibh et tincidunt nibh pharetra. Nisl ultricies augue integer porta mattis. Commodo amet bibendum nulla elit. Neque purus faucibus nam quisque mattis egestas. Porta sollicitudin egestas viverra eget sit fermentum. Auctor in urna sed lacus facilisi. Cursus integer sem gravida urna id mattis ut lectus. Sed pretium tristique faucibus aliquam aenean velit sed diam. Laoreet ipsum habitant arcu non neque pellentesque quam integer adipiscing. Felis tristique lobortis semper ornare dictum eu odio aliquam vestibulum. Elit at est purus elementum commodo nibh. Non blandit id lacus pellentesque in natoque tellus nisi. Vulputate sed donec ultrices nulla tristique amet.",
+      "Ac arcu libero risus iaculis gravida placerat erat leo at. Cras proin varius vulputate gravida. Lorem pharetra tristique sagittis commodo aliquam at. Vestibulum aliquam lectus quis erat viverra neque interdum. Nunc auctor viverra dolor quis malesuada. Fames enim aliquet elementum nisl volutpat cursus convallis morbi commodo. Mauris nunc quis in pellentesque mi pulvinar id eget cras. Gravida adipiscing nunc proin turpis arcu. Suspendisse lacus justo vestibulum purus. Aliquet commodo lacus amet nulla sed elementum scelerisque purus. Sed id tristique auctor magna pretium hac semper. ",
+      "praesent.",
+    ],
+  },
+
+  {
+    title: "DEFINITIONS",
+    paragraph: [
+      "Dolor fringilla nunc nibh et tincidunt nibh pharetra. Nisl ultricies augue integer porta mattis. Commodo amet bibendum nulla elit. Neque purus faucibus nam quisque mattis egestas. Porta sollicitudin egestas viverra eget sit fermentum. Auctor in urna sed lacus facilisi. Cursus integer sem gravida urna id mattis ut lectus. Sed pretium tristique faucibus aliquam aenean velit sed diam. Laoreet ipsum habitant arcu non neque pellentesque quam integer adipiscing. Felis tristique lobortis semper ornare dictum eu odio aliquam vestibulum. Elit at est purus elementum commodo nibh. Non blandit id lacus pellentesque in natoque tellus nisi. Vulputate sed donec ultrices nulla tristique amet.",
+      "Ac arcu libero risus iaculis gravida placerat erat leo at. Cras proin varius vulputate gravida. Lorem pharetra tristique sagittis commodo aliquam at. Vestibulum aliquam lectus quis erat viverra neque interdum. Nunc auctor viverra dolor quis malesuada. Fames enim aliquet elementum nisl volutpat cursus convallis morbi commodo. Mauris nunc quis in pellentesque mi pulvinar id eget cras. Gravida adipiscing nunc proin turpis arcu. Suspendisse lacus justo vestibulum purus. Aliquet commodo lacus amet nulla sed elementum scelerisque purus. Sed id tristique auctor magna pretium hac semper. ",
+      "praesent.",
+    ],
+  },
+  {
+    title: "ELIGIBILITY",
+    paragraph: [
+      "Dolor fringilla nunc nibh et tincidunt nibh pharetra. Nisl ultricies augue integer porta mattis. Commodo amet bibendum nulla elit. Neque purus faucibus nam quisque mattis egestas. Porta sollicitudin egestas viverra eget sit fermentum. Auctor in urna sed lacus facilisi. Cursus integer sem gravida urna id mattis ut lectus. Sed pretium tristique faucibus aliquam aenean velit sed diam. Laoreet ipsum habitant arcu non neque pellentesque quam integer adipiscing. Felis tristique lobortis semper ornare dictum eu odio aliquam vestibulum. Elit at est purus elementum commodo nibh. Non blandit id lacus pellentesque in natoque tellus nisi. Vulputate sed donec ultrices nulla tristique amet.",
+      "Ac arcu libero risus iaculis gravida placerat erat leo at. Cras proin varius vulputate gravida. Lorem pharetra tristique sagittis commodo aliquam at. Vestibulum aliquam lectus quis erat viverra neque interdum. Nunc auctor viverra dolor quis malesuada. Fames enim aliquet elementum nisl volutpat cursus convallis morbi commodo. Mauris nunc quis in pellentesque mi pulvinar id eget cras. Gravida adipiscing nunc proin turpis arcu. Suspendisse lacus justo vestibulum purus. Aliquet commodo lacus amet nulla sed elementum scelerisque purus. Sed id tristique auctor magna pretium hac semper. ",
+      "praesent.",
+    ],
+  },
+  {
+    title: "USE OF THE PLATFORM",
+    paragraph: [
+      "Dolor fringilla nunc nibh et tincidunt nibh pharetra. Nisl ultricies augue integer porta mattis. Commodo amet bibendum nulla elit. Neque purus faucibus nam quisque mattis egestas. Porta sollicitudin egestas viverra eget sit fermentum. Auctor in urna sed lacus facilisi. Cursus integer sem gravida urna id mattis ut lectus. Sed pretium tristique faucibus aliquam aenean velit sed diam. Laoreet ipsum habitant arcu non neque pellentesque quam integer adipiscing. Felis tristique lobortis semper ornare dictum eu odio aliquam vestibulum. Elit at est purus elementum commodo nibh. Non blandit id lacus pellentesque in natoque tellus nisi. Vulputate sed donec ultrices nulla tristique amet.",
+      "Ac arcu libero risus iaculis gravida placerat erat leo at. Cras proin varius vulputate gravida. Lorem pharetra tristique sagittis commodo aliquam at. Vestibulum aliquam lectus quis erat viverra neque interdum. Nunc auctor viverra dolor quis malesuada. Fames enim aliquet elementum nisl volutpat cursus convallis morbi commodo. Mauris nunc quis in pellentesque mi pulvinar id eget cras. Gravida adipiscing nunc proin turpis arcu. Suspendisse lacus justo vestibulum purus. Aliquet commodo lacus amet nulla sed elementum scelerisque purus. Sed id tristique auctor magna pretium hac semper. ",
+      "praesent.",
+    ],
+  },
+  {
+    title: "FUNDRAISER RESPONSIBILITIES",
+    paragraph: [
+      "Dolor fringilla nunc nibh et tincidunt nibh pharetra. Nisl ultricies augue integer porta mattis. Commodo amet bibendum nulla elit. Neque purus faucibus nam quisque mattis egestas. Porta sollicitudin egestas viverra eget sit fermentum. Auctor in urna sed lacus facilisi. Cursus integer sem gravida urna id mattis ut lectus. Sed pretium tristique faucibus aliquam aenean velit sed diam. Laoreet ipsum habitant arcu non neque pellentesque quam integer adipiscing. Felis tristique lobortis semper ornare dictum eu odio aliquam vestibulum. Elit at est purus elementum commodo nibh. Non blandit id lacus pellentesque in natoque tellus nisi. Vulputate sed donec ultrices nulla tristique amet.",
+      "Ac arcu libero risus iaculis gravida placerat erat leo at. Cras proin varius vulputate gravida. Lorem pharetra tristique sagittis commodo aliquam at. Vestibulum aliquam lectus quis erat viverra neque interdum. Nunc auctor viverra dolor quis malesuada. Fames enim aliquet elementum nisl volutpat cursus convallis morbi commodo. Mauris nunc quis in pellentesque mi pulvinar id eget cras. Gravida adipiscing nunc proin turpis arcu. Suspendisse lacus justo vestibulum purus. Aliquet commodo lacus amet nulla sed elementum scelerisque purus. Sed id tristique auctor magna pretium hac semper. ",
+      "praesent.",
+    ],
+  },
+  {
+    title: "DONATIONS & FEES",
+    paragraph: [
+      "Dolor fringilla nunc nibh et tincidunt nibh pharetra. Nisl ultricies augue integer porta mattis. Commodo amet bibendum nulla elit. Neque purus faucibus nam quisque mattis egestas. Porta sollicitudin egestas viverra eget sit fermentum. Auctor in urna sed lacus facilisi. Cursus integer sem gravida urna id mattis ut lectus. Sed pretium tristique faucibus aliquam aenean velit sed diam. Laoreet ipsum habitant arcu non neque pellentesque quam integer adipiscing. Felis tristique lobortis semper ornare dictum eu odio aliquam vestibulum. Elit at est purus elementum commodo nibh. Non blandit id lacus pellentesque in natoque tellus nisi. Vulputate sed donec ultrices nulla tristique amet.",
+      "Ac arcu libero risus iaculis gravida placerat erat leo at. Cras proin varius vulputate gravida. Lorem pharetra tristique sagittis commodo aliquam at. Vestibulum aliquam lectus quis erat viverra neque interdum. Nunc auctor viverra dolor quis malesuada. Fames enim aliquet elementum nisl volutpat cursus convallis morbi commodo. Mauris nunc quis in pellentesque mi pulvinar id eget cras. Gravida adipiscing nunc proin turpis arcu. Suspendisse lacus justo vestibulum purus. Aliquet commodo lacus amet nulla sed elementum scelerisque purus. Sed id tristique auctor magna pretium hac semper. ",
+      "praesent.",
+    ],
+  },
+  {
+    title: "PAYMENTS & WITHDRAWALS",
+    paragraph: [
+      "Dolor fringilla nunc nibh et tincidunt nibh pharetra. Nisl ultricies augue integer porta mattis. Commodo amet bibendum nulla elit. Neque purus faucibus nam quisque mattis egestas. Porta sollicitudin egestas viverra eget sit fermentum. Auctor in urna sed lacus facilisi. Cursus integer sem gravida urna id mattis ut lectus. Sed pretium tristique faucibus aliquam aenean velit sed diam. Laoreet ipsum habitant arcu non neque pellentesque quam integer adipiscing. Felis tristique lobortis semper ornare dictum eu odio aliquam vestibulum. Elit at est purus elementum commodo nibh. Non blandit id lacus pellentesque in natoque tellus nisi. Vulputate sed donec ultrices nulla tristique amet.",
+      "Ac arcu libero risus iaculis gravida placerat erat leo at. Cras proin varius vulputate gravida. Lorem pharetra tristique sagittis commodo aliquam at. Vestibulum aliquam lectus quis erat viverra neque interdum. Nunc auctor viverra dolor quis malesuada. Fames enim aliquet elementum nisl volutpat cursus convallis morbi commodo. Mauris nunc quis in pellentesque mi pulvinar id eget cras. Gravida adipiscing nunc proin turpis arcu. Suspendisse lacus justo vestibulum purus. Aliquet commodo lacus amet nulla sed elementum scelerisque purus. Sed id tristique auctor magna pretium hac semper. ",
+      "praesent.",
+    ],
+  },
+  {
+    title: "VERIFICATION & SECURITY",
+    paragraph: [
+      "Dolor fringilla nunc nibh et tincidunt nibh pharetra. Nisl ultricies augue integer porta mattis. Commodo amet bibendum nulla elit. Neque purus faucibus nam quisque mattis egestas. Porta sollicitudin egestas viverra eget sit fermentum. Auctor in urna sed lacus facilisi. Cursus integer sem gravida urna id mattis ut lectus. Sed pretium tristique faucibus aliquam aenean velit sed diam. Laoreet ipsum habitant arcu non neque pellentesque quam integer adipiscing. Felis tristique lobortis semper ornare dictum eu odio aliquam vestibulum. Elit at est purus elementum commodo nibh. Non blandit id lacus pellentesque in natoque tellus nisi. Vulputate sed donec ultrices nulla tristique amet.",
+      "Ac arcu libero risus iaculis gravida placerat erat leo at. Cras proin varius vulputate gravida. Lorem pharetra tristique sagittis commodo aliquam at. Vestibulum aliquam lectus quis erat viverra neque interdum. Nunc auctor viverra dolor quis malesuada. Fames enim aliquet elementum nisl volutpat cursus convallis morbi commodo. Mauris nunc quis in pellentesque mi pulvinar id eget cras. Gravida adipiscing nunc proin turpis arcu. Suspendisse lacus justo vestibulum purus. Aliquet commodo lacus amet nulla sed elementum scelerisque purus. Sed id tristique auctor magna pretium hac semper. ",
+      "praesent.",
+    ],
+  },
+  {
+    title: "PROHIBITED CAMPAIGNS",
+    paragraph: [
+      "Dolor fringilla nunc nibh et tincidunt nibh pharetra. Nisl ultricies augue integer porta mattis. Commodo amet bibendum nulla elit. Neque purus faucibus nam quisque mattis egestas. Porta sollicitudin egestas viverra eget sit fermentum. Auctor in urna sed lacus facilisi. Cursus integer sem gravida urna id mattis ut lectus. Sed pretium tristique faucibus aliquam aenean velit sed diam. Laoreet ipsum habitant arcu non neque pellentesque quam integer adipiscing. Felis tristique lobortis semper ornare dictum eu odio aliquam vestibulum. Elit at est purus elementum commodo nibh. Non blandit id lacus pellentesque in natoque tellus nisi. Vulputate sed donec ultrices nulla tristique amet.",
+      "Ac arcu libero risus iaculis gravida placerat erat leo at. Cras proin varius vulputate gravida. Lorem pharetra tristique sagittis commodo aliquam at. Vestibulum aliquam lectus quis erat viverra neque interdum. Nunc auctor viverra dolor quis malesuada. Fames enim aliquet elementum nisl volutpat cursus convallis morbi commodo. Mauris nunc quis in pellentesque mi pulvinar id eget cras. Gravida adipiscing nunc proin turpis arcu. Suspendisse lacus justo vestibulum purus. Aliquet commodo lacus amet nulla sed elementum scelerisque purus. Sed id tristique auctor magna pretium hac semper. ",
+      "praesent.",
+    ],
+  },
+];
+
 export default function PrivacyPolicyPage() {
   return (
-      <div className="w-full bg-white font-montserrat">
-      {/* Header Section as per image_fe771b */}
-      <section className="bg-[#FCE9E9] py-24 px-6 text-center">
-        <h1 className="font-extrabold text-[48px] md:text-[56px] text-[#0B112B] mb-4">
-          Privacy policy
-        </h1>
-        <p className="font-normal text-[16px] md:text-[18px] text-[#878FA4] max-w-2xl mx-auto leading-relaxed">
-            Learn how we collect, use, and protect your personal information while ensuring privacy, security, and transparency across all services.
-        </p>
-      </section>
-
-      {/* Content Section with Dividers */}
-      <section className="py-20 px-6 md:px-32">
-        <div className="max-w-4xl mx-auto">
-          
-          {/* Section 1: Introduction */}
-          <div className="border-b border-gray-200 py-10 first:pt-0">
-            <h2 className="font-extrabold text-[24px] text-[#0B112B] mb-6 uppercase">
-              Introduction
-            </h2>
-            <div className="space-y-6 text-[#525252] font-normal text-[16px] leading-[1.8]">
-              <p>
-                Welcome to Cargoland Africa. These Terms and Conditions govern your use of our website, mobile application, and shipping services. By accessing and using our platform, you agree to be bound by these terms. If you do not agree with any part of these terms, please do not use our services.
-              </p>
-              <p>
-                Cargoland Africa is committed to providing reliable, efficient, and transparent shipping solutions across Africa and internationally. We strive to maintain the highest standards of service quality and customer satisfaction.
-              </p>
-            </div>
-          </div>
-
-          {/* Section 2: Definitions */}
-          <div className="border-b border-gray-200 py-10">
-            <h2 className="font-extrabold text-[24px] text-[#0B112B] mb-6 uppercase">
-              Definitions
-            </h2>
-            <div className="space-y-6 text-[#525252] font-normal text-[16px] leading-[1.8]">
-              <p>
-                Cras proin varius vulputate gravida. Lorem pharetra tristique sagittis commodo aliquam at. Vestibulum aliquam lectus quis erat viverra neque interdum. Nunc auctor viverra dolor quis malesuada. Fames enim aliquet elementum nisl volutpat cursus convallis morbi commodo. Mauris nunc quis in pellentesque mi pulvinar id eget cras.
-              </p>
-            </div>
-          </div>
-
-          {/* Section 3: User Responsibilities */}
-          <div className="border-b border-gray-200 py-10">
-            <h2 className="font-extrabold text-[24px] text-[#0B112B] mb-6 uppercase">
-              User Responsibilities
-            </h2>
-            <div className="space-y-6 text-[#525252] font-normal text-[16px] leading-[1.8]">
-              <p>
-                Adipiscing felis tristique lobortis semper ornare dictum eu odio aliquam vestibulum. Elit at est purus elementum commodo nibh. Non blandit id lacus pellentesque in natoque tellus nisi. Vulputate sed donec ultrices nulla tristique amet.
-              </p>
-              <ul className="list-disc list-inside space-y-3 ml-2">
-                <li>Provide accurate and complete information when booking shipments</li>
-                <li>Comply with all applicable laws and regulations</li>
-                <li>Not ship prohibited or illegal items</li>
-              </ul>
-            </div>
-          </div>
-
-          {/* Contact Section - Clean footer of the text */}
-          <div className="py-10">
-            <h2 className="font-extrabold text-[24px] text-[#0B112B] mb-6 uppercase">
-              Contact Us
-            </h2>
-            <p className="text-[#525252] mb-6">
-              If you have any questions about these Terms, please contact us:
-            </p>
-            <div className="space-y-2 text-[16px]">
-              <p><span className="font-bold text-[#0B112B]">Email:</span> <a href="mailto:info@cargoland.africa" className="text-[#E32027] hover:underline">info@cargoland.africa</a></p>
-              <p><span className="font-bold text-[#0B112B]">Phone:</span> (081) 2763-1001</p>
-            </div>
-          </div>
-
+    <div>
+      <section className="padding-x bg-primary-light ">
+        <div className="max-w-[386px] md:max-w-[613px] mx-auto max-md:py-15 md:pt-30 md:pb-31.25 ">
+          <h1 className="text-2xl md:text-[60px] font-extrabold leading-8 md:leading-[72px] text-center">
+            Privacy Policy
+          </h1>
+          <p className="mt-2 text-sm md:text-lg font-light leading-5.5 md:leading-7 max-w-[695px] mx-auto text-center">
+            Learn how we collect, use, and protect your personal information
+            while ensuring privacy, security, and transparency across all
+            services.
+          </p>
         </div>
       </section>
+
+      <div className="padding-x py-20 md:py-[78px] space-y-6 md:space-y-4">
+        {policies.map((policy, idx) => (
+          <div key={policy.title}>
+            <h2 className="text-xl md:text-2xl font-bold leading-7 md:leading-8">
+              {policy.title}
+            </h2>
+
+            <div className="mt-2.5 space-y-6 md:space-y-6.75">
+              {policy.paragraph.map((paragraph, idx) => (
+                <p
+                  key={idx}
+                  className="text-base md:text-lg leading-6 mdleading-6.75 text-gray-700"
+                >
+                  {paragraph}
+                </p>
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
